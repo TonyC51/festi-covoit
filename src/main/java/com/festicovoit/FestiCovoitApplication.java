b6f194d285067a9e635d54faace45cd98e6d4e9d
Tony CHEVRIS <tony.chevris@gmail.com>
2019-11-26 17:24:20 +0100

    first commit

---
+++ src/main/java/com/festicovoit/FestiCovoitApplication.java
@@ -0,0 +1,13 @@
+package com.festicovoit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FestiCovoitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FestiCovoitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/festicovoit/FestiCovoitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.festicovoit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FestiCovoitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
